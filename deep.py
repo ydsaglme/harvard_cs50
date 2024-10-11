5f1d6ff7093efcca42ee5e1883c976dcf2ce4ead
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-10-11 16:01:20 +0300

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,8 @@
+answer = input("What is the the Answer to the Great Question of Life, the Universe and Everything? ")
+
+ans = answer.strip().lower()
+
+if ans == "42" or ans == "forty two" or ans == "forty-two":
+    print("Yes")
+else:
+    print("No")
