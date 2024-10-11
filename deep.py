21c0d5da67f8b808e90d0eda13e40e164afbbb26
Osman Batuhan İnalöz <93053512+ydsaglme@users.noreply.github.com>
2024-10-11 16:10:02 +0300

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,5 +1,4 @@
 answer = input("What is the the Answer to the Great Question of Life, the Universe and Everything? ")
-
 ans = answer.strip().lower()
 
 if ans == "42" or ans == "forty two" or ans == "forty-two":
